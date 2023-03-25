a2ac4974bae8db9532c6dc4ca6b1817c09d04450
Ivan Semenov <Tomsk16121@yandex.ru>
2023-03-25 15:37:56 +0700

    updated font of input name

--- VisualNovel/Extensions/UIFont+AppFonts.swift
+++ VisualNovel/Extensions/UIFont+AppFonts.swift
@@ -11,5 +11,5 @@ import UIKit
 extension UIFont {
     static let mainHeader = UIFont.systemFont(ofSize: 40, weight: .bold)
     static let gamePrompt = UIFont.systemFont(ofSize: 19, weight: .bold)
-    static let userNameText = UIFont.systemFont(ofSize: 20)
+    static let userNameText = UIFont(name: "Verdana Italic", size: 20)
 }
